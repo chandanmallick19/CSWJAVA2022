5d9dfaaee910eabc25b7f7a1c5961f2026338dec
CHANDAN KUMAR MALLICK <chandansoumya28@gmail.com>
2023-01-13 09:54:42 +0530

    Updated

--- src/Assignment1/A1Q10.java
+++ src/Assignment1/A1Q10.java
@@ -1,4 +1,23 @@
 package Assignment1;
-
+import java.util.Scanner;
 public class A1Q10 {
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            String number = sc.next();
+            System.out.println("Enter base 1: ");
+            int base1 = sc.nextInt();
+            System.out.println("Enter base 2: ");
+            int base2 = sc.nextInt();
+
+            String converted = convert(number, base1, base2);
+            System.out.println("Converted: " + converted);
+        }
+
+        public static String convert(String number, int base1, int base2) {
+            int decimal = Integer.parseInt(number, base1);
+            String converted = Integer.toString(decimal, base2);
+            return converted;
+        }
+    }
+
